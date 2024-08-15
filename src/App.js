9d1bbaf0dc7b04957408ac8315df8b72844df8b5
“zascia <zascia@ukr.net>
2024-08-15 13:33:41 +0200

    try to see netlify

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Here we setup automatic deploy to Netlify
+          Edit <code>src/App.js</code> and save to reload. Here we setup
+          automatic deploy to Netlify
         </p>
         <a
           className="App-link"
